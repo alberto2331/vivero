6234fb2df50ae445f6624a80dab6b63a4917be53
Alberto Saiz <saizguidarelli@gmail.com>
2021-11-09 19:41:18 -0300

    primer commit

---
+++ src/test/java/com/vivero/ViveroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViveroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
