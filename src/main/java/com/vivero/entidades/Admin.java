d9a525878ff017651ac581ec50642fb36e600733
Fernando Lerner <fernandoelerner@gmail.com>
2021-11-10 21:17:20 -0300

    cambios en entidad admin

--- src/main/java/com/vivero/entidades/Admin.java
+++ src/main/java/com/vivero/entidades/Admin.java
@@ -1,8 +1,68 @@
-
-
 package com.vivero.entidades;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
 public class Admin {
 
+    @Id
+    private String id;
+    private String name;
+    private String lastName;
+    private String user;
+    private String password;
+
+    public Admin() {
+    }
+
+    public Admin(String id, String name, String lastName, String user, String password) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.user = user;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
 }
