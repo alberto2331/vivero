56e16d3fd0ed3aadc8e8b37d8157451c8457df8c
Alberto Saiz <saizguidarelli@gmail.com>
2021-11-15 21:45:37 -0300

    vista provisoria carga de producto-planta

---
+++ src/main/java/com/vivero/repositorios/PlantaRepositorio.java
@@ -0,0 +1,10 @@
+
+package com.vivero.repositorios;
+
+import com.vivero.entidades.Planta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface PlantaRepositorio extends JpaRepository <Planta, String> {
+
+}
