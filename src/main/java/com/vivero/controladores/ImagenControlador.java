9cc34a092a826dd403c1266440254493528543c5
Juan Guardiola <jguardiolagarcia@gmail.com>
2021-11-30 21:57:39 -0300

    commit para ver galeria de fotos

--- src/main/java/com/vivero/controladores/ImagenControlador.java
+++ src/main/java/com/vivero/controladores/ImagenControlador.java
@@ -5,16 +5,23 @@
  */
 package com.vivero.controladores;
 
+import com.vivero.entidades.Foto;
 import com.vivero.entidades.Planta;
 import com.vivero.errores.ErrorServicio;
+import com.vivero.repositorios.FotoRepositorio;
 import com.vivero.repositorios.PlantaRepositorio;
+import com.vivero.servicios.FotoServicio;
 import com.vivero.servicios.PlantaServicio;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,29 +29,53 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/imagen")
 public class ImagenControlador {
-    
+
     @Autowired
     private PlantaRepositorio plantaRepositorio;
-    
+
+    @Autowired
+    private FotoServicio fotoServicio;
+
     @GetMapping("/planta/{id}")
     public ResponseEntity<byte[]> getByProducto(@PathVariable String id) throws ErrorServicio {
         try {
             Planta planta = plantaRepositorio.getById(id);
-            
+
             if (planta.getPortada() == null) {
                 throw new ErrorServicio("El producto no tiene una imagen.");
             }
             byte[] imagen = planta.getPortada().getContenido();
-            
+
             HttpHeaders headers = new HttpHeaders();
-            
+
             headers.setContentType(MediaType.IMAGE_JPEG);
 
             return new ResponseEntity(imagen, headers, HttpStatus.OK);
         } catch (Exception e) {
             throw new ErrorServicio("Hubo un problema al cargar la imagen del producto");
-        }   
+        }
     }
-    
-}
+//Listar fotos de galeria
+    @GetMapping("/galeria/{id}")
+    public String getGaleriaByProducto(@PathVariable String id, Model model) throws ErrorServicio {
+        try {
+            System.out.println("Id: " + id);
+            List<Foto> fotos = fotoServicio.listarFotosDeProducto(id);
+            System.out.println("Fotos: " + fotos.toString());
+            
+            List<String> datosFotos = new ArrayList();
+            
+            for (Foto foto : fotos) {
+                String base64 = Base64.encodeBase64String(foto.getContenido());
+                datosFotos.add(base64);
+            }
+            
+            model.addAttribute("listaFotos",datosFotos);
+            
+            return "planta-ver";
+        } catch (Exception e) {
+            throw new ErrorServicio("Hubo un problema al cargar las imagenes del producto");
+        }
+    }
 
+}
--- src/main/java/com/vivero/repositorios/FotoRepositorio.java
+++ src/main/java/com/vivero/repositorios/FotoRepositorio.java
@@ -3,10 +3,14 @@ package com.vivero.repositorios;
 import com.vivero.entidades.Foto;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.multipart.MultipartFile;
 
-public interface FotoRepositorio extends JpaRepository <Foto, String>{
+public interface FotoRepositorio extends JpaRepository<Foto, String> {
 
     public Foto save(List<MultipartFile> galeria);
 
+    //cambios introducidos por JG para traer lista de fotos
+    @Query("select c from Foto c where c.producto.id like :id ")
+    public List<Foto> listaFotosDeProducto(String id);
 }
