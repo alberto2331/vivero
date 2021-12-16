bf782b95c601d95f4ea8d18e7c119b75208cd4d5
Dana Cozzi <dana.cozzi@gmail.com>
2021-12-15 21:50:59 -0300

    Productos destacados index

---
+++ src/main/java/com/vivero/controladores/IndexControlador.java
@@ -0,0 +1,65 @@
+package com.vivero.controladores;
+
+import com.vivero.entidades.Foto;
+/*Importado momentaneamente para poder mostrar imagenes de productos a modificar
+Entiendo que cuando juntemos las ramas de front y back podremos borrar esta importacion y usar el fotoServicio de Juan Guardiola 
+ */
+import com.vivero.entidades.Planta;
+import com.vivero.entidades.Portada;
+import com.vivero.entidades.Producto;
+import com.vivero.errores.ErrorServicio;
+import com.vivero.repositorios.FotoRepositorio;/*Importado momentaneamente para poder mostrar imagenes de productos a modificar
+Entiendo que cuando juntemos las ramas de front y back podremos borrar esta importacion y usar el fotoServicio de Juan Guardiola
+ */
+import com.vivero.repositorios.ProductoRepositorio;
+//import com.vivero.enumeraciones.Luz;
+//import com.vivero.enumeraciones.Tamanio;
+//import com.vivero.enumeraciones.Ubicacion;
+//import com.vivero.repositorios.PlantaRepositorio;
+import com.vivero.servicios.PlantaServicio;
+import com.vivero.servicios.ProductoServicio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.transaction.Transactional;
+
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.apache.tomcat.util.json.JSONParser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.json.*;
+@Controller
+@RequestMapping("/")
+public class IndexControlador {
+
+    @Autowired
+    private ProductoRepositorio productoRepositorio;
+
+    
+    @GetMapping("/")
+    public String plantaDestacada( ModelMap modelo) {
+                 
+            Boolean destacado;
+            destacado = true;
+          
+            List<Producto> plantasFiltradas = productoRepositorio.listarProductosDestacados(destacado);
+            modelo.put("plantasFiltradas", plantasFiltradas);
+            return "index";
+        }
+        
+    }
+    
+    
--- src/main/java/com/vivero/repositorios/ProductoRepositorio.java
+++ src/main/java/com/vivero/repositorios/ProductoRepositorio.java
@@ -1,11 +1,19 @@
 package com.vivero.repositorios;
 
 
+
+
 import com.vivero.entidades.Producto;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductoRepositorio extends JpaRepository <Producto, String>{
-    
+
+    @Query("select c from Producto c where c.destacado =:destacado")
+	public List<Producto> listarProductosDestacados(Boolean destacado);
+	
 
 }
