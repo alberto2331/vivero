77c2a609994abe5e62efcdbc737275ed2ad4b41a
Juan Guardiola <jguardiolagarcia@gmail.com>
2021-12-16 20:54:05 -0300

    juan fix vista productos

--- src/main/java/com/vivero/controladores/IndexControlador.java
+++ src/main/java/com/vivero/controladores/IndexControlador.java
@@ -48,7 +48,6 @@ public class IndexControlador {
     @Autowired
     private ProductoRepositorio productoRepositorio;
 
-    
     @GetMapping("/")
     public String plantaDestacada( ModelMap modelo) {
                  
